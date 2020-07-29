15cfd44aa544f199f93519317a9bad121043bd0d
Pritivi Chhabria Sachin <pritivischhabria@berkeley.edu>
2020-07-29 23:05:57 +0530

    Project Completed using Starter Code.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,26 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5 * sender.alpha
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2) {
+            sender.alpha = sender.alpha * 2
+        }
+        playSound(name: sender.currentTitle!)
+    }
     
-
+    func playSound(name: String!) {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
